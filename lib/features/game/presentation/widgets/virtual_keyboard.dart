59bffa8af1f5d3c41fb608ef75a9bddd9e0e5c65
desodre <jhonathacirilo9000@gmail.com>
2026-05-26 19:25:06 -0400

    (#13) moved enter and backspace button to improve better usability.

--- lib/features/game/presentation/widgets/virtual_keyboard.dart
+++ lib/features/game/presentation/widgets/virtual_keyboard.dart
@@ -10,8 +10,8 @@ class VirtualKeyboard extends StatelessWidget {
 
   static const List<List<String>> _rows = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-    ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '⌫'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L','⌫'],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'ENTER'],
   ];
 
   @override
